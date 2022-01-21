96d84ce5e341d925faeb9b34af63675ad9ca0853
lbcred <lewis.battersby@credera.co.uk>
2022-01-21 16:44:47 +0000

    removes client secret variable from terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,8 +2,3 @@ variable "environment_prefix" {
   type        = string
   description = "The name of the environment to deploy the resource."
 }
-
-variable "client_secret" {
-  type         = string
-  description = "The client secret used to authenticate to Azure AD"
-}
